a5334bbf51547ac8f4a8b08b9b8b307309432dd0
Theeaglefreak <soumyaranjansamal2420@gmail.com>
2021-05-01 22:34:42 +0530

    Scenario1 code push

--- src/test/java/Utilities/DemoClass.java
+++ src/test/java/Utilities/DemoClass.java
@@ -4,7 +4,7 @@ public class DemoClass {
 
    public static void main(String args[])
    {
-       System.out.println("Hi");
+       System.out.println("Changng this code just to see how branch reacts");
 
    }
 }
